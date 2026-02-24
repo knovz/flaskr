84664673c37187e61ec39b5d1013f7b9d6073e37
KnoVz <h@knovz.com>
2026-02-24 14:04:37 +0100

    blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from . import db
 from . import auth
+from . import blog
 
 
 def create_app(test_config=None):
@@ -30,5 +31,8 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
+    # we associate index, and blog.index, with /
 
     return app
